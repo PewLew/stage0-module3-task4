c36abd9efc751a5a32f3fc53c2d102d3035e174c
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-11-30 15:06:58 +0600

    a

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int num1 = 99;
+        int num2 = 104;
+        int num3 = 97;
+        int num4 = 105;
+        int num5 = 114;
+
+        char c = (char)num1;
+        char h = (char)num2;
+        char a = (char)num3;
+        char i = (char)num4;
+        char r = (char)num5;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(r);
+
     }
 }
